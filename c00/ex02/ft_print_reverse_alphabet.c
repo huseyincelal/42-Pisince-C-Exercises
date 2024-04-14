bba5f72c0a33ef2e9c683ae001e7060fcd59a0df
huseyinCelal <huseyincelalgozlu@gmail.com>
2024-04-14 23:38:53 +0300

    dewam

--- c00/ex02/ft_print_reverse_alphabet.c
+++ c00/ex02/ft_print_reverse_alphabet.c
@@ -1,12 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_bilmemne.c                                      :+:      :+:    :+:   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: hcgozlu <huseyincelalgozlu@gmail.com>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/14 02:59:46 by hcgozlu           #+#    #+#             */
-/*   Updated: 2024/04/14 02:59:46 by hcgozlu          ###   ########.fr       */
+/*   Created: 2024/04/14 23:33:42 by hcgozlu           #+#    #+#             */
+/*   Updated: 2024/04/14 23:33:42 by hcgozlu          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	 c;
+
+	c = 'z';
+	while (c >= 'a')
+	{
+		write (1, &c, 1);
+		 c--;
+	}
+}
